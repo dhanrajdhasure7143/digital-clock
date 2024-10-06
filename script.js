f4869b97a58ac95206e1514f59eaf347ce6b3ab9
dhanraj.hasure <dhanraj.hasure@epsoftinc.com>
2024-10-06 23:31:44 +0530

    added documentation for the code

--- script.js
+++ script.js
@@ -5,9 +5,22 @@ const timeEl = document.querySelector('.time')
 const dateEl = document.querySelector('.date')
 const toggle = document.querySelector('.toggle')
 
+/**
+ * An array of the days of the week
+ * @type {string[]}
+ */
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+/**
+ * An array of the months of the year
+ * @type {string[]}
+ */
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
+/**
+ * Toggles the dark mode
+ * @param {MouseEvent} e - The click event
+ */
 toggle.addEventListener('click', (e) => {
     const html = document.querySelector('html')
     if (html.classList.contains('dark')) {
@@ -19,6 +32,9 @@ toggle.addEventListener('click', (e) => {
     }
 })
 
+/**
+ * Sets the clock hands to the correct positions, and updates the time and date display elements.
+ */
 function setTime() {
     const time = new Date();
     const month = time.getMonth()
@@ -38,10 +54,37 @@ function setTime() {
     dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
 }
 
+/**
+ * Scales a number from one range to another range.
+ * @param {number} num - The number to scale
+ * @param {number} in_min - The minimum of the input range
+ * @param {number} in_max - The maximum of the input range
+ * @param {number} out_min - The minimum of the output range
+ * @param {number} out_max - The maximum of the output range
+ * @returns {number} The scaled number
+ */
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+/**
+ * Initializes the clock by setting it to the current time, and starts the interval which will update the clock every second.
+ */
 setTime()
 
+/**
+ * Starts an interval which will call the setTime function every second, thus keeping the clock up to date.
+ */
 setInterval(setTime, 1000)
+
+/**
+ * Logs the author of the program to the console.
+ */
+function author() {
+    console.log('Created with  by Dhanraj D. Hasure');
+}
+
+/**
+ * Calls the author function.
+ */
+author()
